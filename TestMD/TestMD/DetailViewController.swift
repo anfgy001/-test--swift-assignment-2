fa7a408cd2b75ceea2660b40ca95e0e66b99d9e3
Gianni Anfiteatro <anfgy001@mymail.unisa.edu.au>
2017-10-14 17:33:40 +1030

    Centered menu items
    Centered menu items

--- TestMD/TestMD/DetailViewController.swift
+++ TestMD/TestMD/DetailViewController.swift
@@ -12,8 +12,11 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
+    @IBOutlet weak var pageLabel: UILabel!
 
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var detailNavigationItem: UINavigationItem!
     
     func configureView() {
         // Update the user interface for the detail item.
@@ -27,8 +30,17 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        let screenSize = UIScreen.main.bounds;
+        let screenWidth = screenSize.width;
+        //var thePageLabel = pageLabel.text!;
+        pageLabel.center.x = screenWidth / 2;
+        imageView.center.x = screenWidth / 2;
+        detailDescriptionLabel.center.x = screenWidth / 2;
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
